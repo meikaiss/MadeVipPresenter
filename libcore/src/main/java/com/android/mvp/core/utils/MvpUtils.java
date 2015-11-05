a5e95f57c326cb56168369df5afb8e22df84ca15
meikai <347287812@qq.com>
2015-11-06 00:15:44 +0800

    增加空引用判断工具类

--- libcore/src/main/java/com/android/mvp/core/utils/MvpUtils.java
+++ libcore/src/main/java/com/android/mvp/core/utils/MvpUtils.java
@@ -4,6 +4,9 @@ import android.widget.Toast;
 
 import com.android.mvp.core.MvpCore;
 
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Created by meikai on 15/11/4.
  */
@@ -11,5 +14,30 @@ public class MvpUtils {
 
     public static void showToast(String msg){
         Toast.makeText(MvpCore.getApplication(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public static boolean isEmpty(String s) {
+        return s == null || s.trim().length() == 0;
+    }
+
+    public static boolean isEmpty(Collection<?> c) {
+        return c == null || c.isEmpty();
+    }
+
+
+    public static boolean isEmpty(Map<?, ?> map) {
+        return map == null || map.isEmpty();
+    }
+
+    public static boolean isNotEmpty(String s) {
+        return !isEmpty(s);
+    }
+
+    public static boolean isNotEmpty(Collection<?> c) {
+        return !isEmpty(c);
+    }
+
+    public static boolean isNotEmpty(Map<?, ?> map) {
+        return !isEmpty(map);
     }
 }
